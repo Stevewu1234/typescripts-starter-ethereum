f34726ecb7cb9b806b17b46eddec53b492e82e1f
Stevewu1234 <plgawu@163.com>
2022-10-03 23:06:21 +0800

    update template environment

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -23,10 +23,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    main: {
+      url: process.env.MAINNET_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
@@ -34,6 +39,7 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP,
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
